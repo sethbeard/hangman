e2599f43e37aa53c524af6bda16a156629134d96
sethbeard <70112744+sethbeard@users.noreply.github.com>
2020-09-29 00:37:37 -0400

    Update mainui.java

--- mainui.java
+++ mainui.java
@@ -41,11 +41,10 @@ public class mainui {
 		while (newGame.getWrongGuesses() < 6) {
 			clearScreen();
 			printState(newGame.getWrongGuesses());
-			System.out.println("\n\n");
-			System.out.println(newGame.getGuess());
-			System.out.println("");
-			System.out.println("\nLetters Guessed: " + guessedLetters);
-			System.out.println("Enter Guess: ");
+			System.out.println("\n\n\n" +
+			newGame.getGuess() +
+			"\n\nLetters Guessed: " + guessedLetters +
+			"\nEnter Guess: ");
 			String newGuess = userInput.nextLine().toLowerCase();
 			if (newGuess.equals(null) || !newGuess.equals("")) {
 				char guess = newGuess.charAt(0);
